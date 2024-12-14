466f020de164bc8a8a6475dc71a954fc73dd2b1c
BuddhiSankalpa <bsankalpa500@gmail.com>
2024-12-14 16:49:58 +0530

    add subscribe component and backend integration

--- src/app/views/subscribe/subscribe.component.ts
+++ src/app/views/subscribe/subscribe.component.ts
@@ -1,12 +1,77 @@
-import {Component} from '@angular/core';
-import { pricing_table_id, publishable_key } from "../../../environment/environment";
+import {Component, OnInit} from '@angular/core';
+import {pricing_table_id, publishable_key} from "../../../environment/environment";
+import {ApiService} from "../../service/api.service";
+import {finalize} from "rxjs";
+import {cilCheckCircle} from "@coreui/icons";
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrl: './subscribe.component.scss'
 })
-export class SubscribeComponent {
+export class SubscribeComponent implements OnInit {
   pricingTableId: string = pricing_table_id;
   publishableKey: string = publishable_key;
+
+  loading: boolean = true;
+  subDetails: any = [1,2,3];
+  packageDetails: any = {
+    "silver": [
+      "No adds",
+      "Custom timeframes",
+      "Custom Range Bars",
+      "Volume profile",
+      "Multiple watchlists",
+      "Bar replay",
+      "Indicators on indicators"
+    ],
+    "gold": [
+      "No adds",
+      "Custom timeframes",
+      "Custom Range Bars",
+      "Volume profile",
+      "Multiple watchlists",
+      "Bar replay",
+      "Indicators on indicators",
+      "Volume profile"
+    ],
+    "platinum" : [
+      "No adds",
+      "Custom timeframes",
+      "Custom Range Bars",
+      "Volume profile",
+      "Multiple watchlists",
+      "Bar replay",
+      "Indicators on indicators",
+      "Custom Range Bars",
+      "Indicators on indicators"
+    ]
+  }
+
+  constructor(
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.apiService.getSubscriptionPlan()
+      .pipe(
+        finalize(()=>{
+          this.loading = false;
+        })
+      )
+      .subscribe({
+        next: value => {
+          if (value?.statusCode === 200) {
+            this.subDetails = value?.content;
+          }
+        },
+        error: err => {}
+      })
+  }
+
+  getPackageDetails(planName: string) {
+    return this.packageDetails[planName.toLowerCase()];
+  }
+
+  protected readonly cilCheckCircle = cilCheckCircle;
 }
--- src/app/views/subscribe/subscribe.module.ts
+++ src/app/views/subscribe/subscribe.module.ts
@@ -2,15 +2,35 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SubscribeComponent} from "./subscribe.component";
 import {SubscribeRoutingModule} from "./subscribe-routing.module";
+import {
+  ButtonDirective,
+  CardBodyComponent,
+  CardComponent,
+  CardImgDirective, CardTextDirective, CardTitleDirective,
+  ColComponent,
+  GutterDirective,
+  RowComponent
+} from "@coreui/angular";
+import {IconDirective} from "@coreui/icons-angular";
 
 
 
 @NgModule({
   declarations: [SubscribeComponent],
-  imports: [
-    CommonModule,
-    SubscribeRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        SubscribeRoutingModule,
+        ColComponent,
+        RowComponent,
+        CardComponent,
+        CardBodyComponent,
+        GutterDirective,
+        CardImgDirective,
+        CardTitleDirective,
+        CardTextDirective,
+        ButtonDirective,
+        IconDirective
+    ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SubscribeModule { }
